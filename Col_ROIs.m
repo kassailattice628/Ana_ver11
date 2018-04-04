c6d983ebeb52df6d4aa65d891e354e4922fa37f0
Masatoshi  KASAI <kassailattice628@gmail.com>
2018-04-04 11:16:27 +0900

    add plot roentation map

--- Col_ROIs.m
+++ Col_ROIs.m
@@ -44,8 +44,13 @@ switch sobj.pattern
     case 'Size_rand'
         map_size
         
-    case {'Sin', 'Rect', 'Gabor', 'MoveBar'}
-        map_angler;
+    case {'Sin', 'Rect', 'Gabor'}
+        m
+        map_angler(0, imgBG);
+        
+    case 'MoveBar'
+        map_angler(0, imgBG);
+        map_angler(1, imgBG);
         
     case {'Uni'}
         map_nxn(1);
@@ -60,12 +65,18 @@ end
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     function map_size
         imgBG2 = zeros(img_sz * img_sz, 3);
+        imgBG3 = zeros(img_sz * img_sz, 3);
         %% set colormap based on 1 parameter
-        rois = find(max(imgobj.R_size(:,:,2))>0.15);
-        RGB_list = colormap(jet(7));
+        %rois = find(max(imgobj.R_size(:,:,2))>0.15);
+        rois = find(max(imgobj.R_size(1:5,:,2))>0.15);
+        
+        %RGB_list = colormap(jet(7));
+        RGB_list = colormap(jet(size(imgobj.R_size,1)));
         for i2 = rois
-            [~, best_i_on] = max(imgobj.R_size(:, i2, 1));
-            [~, best_i_off] = max(imgobj.R_size(:, i2, 2));
+            %[~, best_i_on] = max(imgobj.R_size(:, i2, 1));
+            %[~, best_i_off] = max(imgobj.R_size(:, i2, 2));
+            [~, best_i_on] = max(imgobj.R_size(1:5, i2, 1));
+            [~, best_i_off] = max(imgobj.R_size(1:5, i2, 2));
             %ON
             RGB = RGB_list(best_i_on,:);
             imgBG(imgobj.Mask_rois(:,i2),1) = RGB(1);
@@ -76,28 +87,111 @@ end
             imgBG2(imgobj.Mask_rois(:,i2),1) = RGB2(1);
             imgBG2(imgobj.Mask_rois(:,i2),2) = RGB2(2);
             imgBG2(imgobj.Mask_rois(:,i2),3) = RGB2(3);
+            
+            
+            
+            if size(imgobj.R_size, 3) == 3
+                [~, best_i_onoff] = max(imgobj.R_size(1:5, i2, 3));
+                RGB3 = RGB_list(best_i_onoff,:);
+                imgBG3(imgobj.Mask_rois(:,i2),1) = RGB3(1);
+                imgBG3(imgobj.Mask_rois(:,i2),2) = RGB3(2);
+                imgBG3(imgobj.Mask_rois(:,i2),3) = RGB3(3);
+            end
+            
+            
         end
         
         imgBG = reshape(imgBG,[img_sz, img_sz, 3]);
         imgBG2 = reshape(imgBG2,[img_sz, img_sz, 3]);
+        imgBG3 = reshape(imgBG3,[img_sz, img_sz, 3]);
+        
         
         %show image
         figure
         subplot(1,2,1)
         imshow(imgBG)
-        colormap(jet(7))
+        %colormap(jet(7))
+        colormap(jet(size(imgobj.R_size,1)))
         colorbar
         
         subplot(1,2,2)
         imshow(imgBG2)
         colorbar
+        
+        figure
+        imshow(imgBG)
+        figure
+        imshow(imgBG2)
+        
+        figure
+        imshow(imgBG3)
         %}
         
     end
 
 %% %%%%%%%%%%%%%%%%%%%% %%
-    function map_angler
-        %% set colormap based on angler info
+
+    function map_angler(type, imgBG)
+        %type:: directino or orientation
+        
+        switch type
+            case 0 %directino
+                n = 36;
+                h_list = linspace(0, 1, n);
+                angle_list = linspace(0, 2*pi, n);
+                L = imgobj.L_dir;
+                Ang = imgobj.Ang_dir;
+            case 1 %orientation
+                n = 18;
+                h_list = linspace(0, 1, n);
+                angle_list = linspace(-pi/2, pi/2, n);
+                L = imgobj.L_ori;
+                Ang = imgobj.Ang_ori;
+        end
+        
+        rois_selective = find(L > 0.2); 
+        rois_max = max(max(imgobj.dFF_s_ave));
+        rois_valid = find(rois_max > 0.15);
+        rois = intersect(rois_selective, rois_valid);
+        
+        for i2 = rois'
+            %set HSV
+            ang1 = find(angle_list > Ang(i2), 1, 'first');
+            ang2 = find(angle_list <= Ang(i2), 1, 'last');
+            if abs(Ang(i2) - angle_list(ang1)) < abs(Ang(i2) - angle_list(ang2))
+                ang = ang1;
+            else
+                ang = ang2;
+            end
+            h = h_list(ang);
+            if L(i2) > 1, v = 1; else, v = L(i2); end
+            
+            HSV_roi = [h, 1, v];
+            RGB = hsv2rgb(HSV_roi);
+            
+            imgBG(imgobj.Mask_rois(:,i2),1) = RGB(1);
+            imgBG(imgobj.Mask_rois(:,i2),2) = RGB(2);
+            imgBG(imgobj.Mask_rois(:,i2),3) = RGB(3);
+        end
+        imgBG = reshape(imgBG,[img_sz, img_sz, 3]);
+        figure
+        imshow(imgBG)
+        hold on
+        % vector map
+        [U, V] = pol2cart(Ang, L);
+        quiver(imgobj.centroid(1,rois), imgobj.centroid(2,rois), U(rois)*50, -V(rois)*50,...
+            'AutoScale', 'off', 'Color', 'w')
+        axis ij
+        axis([0, img_sz, 0, img_sz])
+        hold off
+        
+        colormap(hsv(n));
+    end
+%%
+%{
+function map_angler
+        %% Direction Selectivity Map
+        % set colormap based on angler info
         % colorized every 10 deg in hue (HSV space), 360/10 = 36
         h_list = linspace(0, 1, 36);
         angle_list = linspace(0, 2*pi, 36);
@@ -111,7 +205,7 @@ end
             %set HSV
             ang1 = find(angle_list > imgobj.Ang_dir(i2), 1, 'first');
             ang2 = find(angle_list <= imgobj.Ang_dir(i2), 1, 'last');
-            if abs(imgobj.Ang_dir(i2)-ang1) < abs(imgobj.Ang_dir(i2) - ang2)
+            if abs(imgobj.Ang_dir(i2)-angle_list(ang1)) < abs(imgobj.Ang_dir(i2) - angle_list(ang2))
                 ang = ang1;
             else
                 ang = ang2;
@@ -134,8 +228,6 @@ end
         imgBG = reshape(imgBG,[img_sz, img_sz, 3]);
         
         figure
-        %subplot(1,2,1)
-        % response map
         imshow(imgBG)
         hold on
         % vector map
@@ -148,18 +240,55 @@ end
         
         colormap(hsv(36));
         
-        %{
-        subplot(1,2,2)
+        %% Orientation Selectivity Map
+        % set colormap based on olientation angle info
+        % colorized evely 10 deg in hue sapce, 180/10 = 18
+        h_list = linspace(0, 1, 18);
+        angle_list = linspace(-pi/2, pi/2, 18);
+        
+        rois_selective = find(imgobj.L_ori > 0.2);
+        rois_max = max(max(imgobj.dFF_s_ave));
+        rois_valid = find(rois_max > 0.15);
+        rois = intersect(rois_selective, rois_valid);
+        
+        for i2 = rois'
+            ang1 = find(angle_list > imgobj.Ang_ori(i2), 1, 'first');
+            ang2 = find(angle_list <= imgobj.Ang_ori(i2), 1, 'last');
+            if abs(imgobj.Ang_ori(i2) - angle_list(ang1)) < abs(imgobj.Ang_ori(i2) - angle_list(ang2))
+                ang = ang1;
+            else
+                ang = ang2;
+            end
+            h = h_list(ang);
+            v = imgobj.L_ori(i2);
+            if imgobj.L_ori (i2) > 1
+                v = 1;
+            end
+            HSV_roi = [h, 1, v];
+            RGB = hsv2rgb(HSV_roi);
+            
+            imgBG(imgobj.Mask_rois(:,i2),1) = RGB(1);
+            imgBG(imgobj.Mask_rois(:,i2),2) = RGB(2);
+            imgBG(imgobj.Mask_rois(:,i2),3) = RGB(3);
+        end
+        
+        imgBG = reshape(imgBG,[img_sz, img_sz, 3]);
+        
+        figure
+        imshow(imgBG)
+        hold on
         % vector map
-        [U, V] = pol2cart(imgobj.Ang_dir, imgobj.L_dir);
+        [U, V] = pol2cart(imgobj.Ang_ori, imgobj.L_ori);
         quiver(imgobj.centroid(1,rois), imgobj.centroid(2,rois), U(rois)*50, -V(rois)*50,...
-            'AutoScale', 'off')
+            'AutoScale', 'off', 'Color', 'w')
         axis ij
         axis([0, 320, 0, 320])
-        axis square
-        set(gca, 'Xticklabel', [], 'YTicklabel', []);
-        %}
+        hold off
+        
+        colormap(hsv(18));
     end
+
+%}
 
 %% %%%%%%%%%%%%%%%%%%%% %%
     function map_nxn(type)
